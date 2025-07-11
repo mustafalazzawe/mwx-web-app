97989a4a836d5438d905ed8d783067e4a97d8b7d
mustafalazzawe <mustafa.alazzawe@gmail.com>
2025-07-11 12:53:26 -0400

    Updating vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/mwx-web-app/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
